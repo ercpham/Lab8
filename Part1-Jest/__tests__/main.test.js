f622779dae66d49f0bd03556291a46380f999f75
ercpham <erc.pham@gmail.com>
2021-02-27 18:04:31 -0800

    Update tests

--- Part1-Jest/__tests__/main.test.js
+++ Part1-Jest/__tests__/main.test.js
@@ -1,16 +1,25 @@
 const formatVolumeIconPath = require("../assets/scripts/main")
 
 describe("Volume: ", () => {
-    test("> 66", () => {
+    test("67", () => {
         expect(formatVolumeIconPath(67)).toMatch(/3/);
     })
-    test("> 33", () => {
+    test("66", () => {
+        expect(formatVolumeIconPath(66)).toMatch(/2/);
+    })
+    test("100", () => {
+        expect(formatVolumeIconPath(100)).toMatch(/3/);
+    })
+    test("34", () => {
         expect(formatVolumeIconPath(34)).toMatch(/2/);
     })
-    test("> 0", () => {
+    test("33", () => {
+        expect(formatVolumeIconPath(33)).toMatch(/1/);
+    })
+    test("1", () => {
         expect(formatVolumeIconPath(1)).toMatch(/1/);
     })
-    test("== 0", () => {
+    test("0", () => {
         expect(formatVolumeIconPath(0)).toMatch(/0/);
     })
 });
